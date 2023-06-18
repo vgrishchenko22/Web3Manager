b8254c218c70dbf97ae9080b34d04559262dcd98
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-06-19 04:49:38 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const WebApp = window.Telegram.WebApp;
 
-WebApp.enableClosingConfirmation();
-async function a2() {
-    // WebApp.MainButton.setParams({"text": "ДА!!!"});
+async function begin() {
+    WebApp.MainButton.setParams({"text": "Connect"});
     WebApp.MainButton.show();
     WebApp.MainButton.enable();
-};
+}
+
 WebApp.MainButton.onClick(() => {
     WebApp.MainButton.showProgress(true);
     setTimeout(() => {
-        WebApp.showAlert("Qwerty");
-        WebApp.MainButton.hideProgress();
+        WebApp.showAlert("Connected!", () => {
+            WebApp.MainButton.hideProgress();
+            WebApp.MainButton.disable();
+            WebApp.MainButton.setParams({"text": "Connect"});
+
+            document.getElementById("connect-section").style.display = "none";
+            document.getElementById("search-section").style.display = "block";
+        });
     }, 5000);
 });
