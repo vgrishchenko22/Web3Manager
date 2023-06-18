704630eb54063cb334d765a19f5dda90828d26c4
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-06-19 01:43:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ async function a1(params) {
     WebApp.showPopup(params);
 };
 async function a2() {
+    WebApp.MainButton.setParams({"text": "Ыыыыыыы"})
+    WebApp.MainButton.show();
+    WebApp.MainButton.enable();
+};
+enableClosingConfirmation();
+WebApp.MainButton.onClick(() => {
+    WebApp.MainButton.showProgress(true)
     setTimeout(() => {
         WebApp.showAlert("Qwerty");
+        WebApp.MainButton.hideProgress()
     }, 5000);
-};
+});
 async function a3() {
     WebApp.showConfirm("Ytrewq!");
 };
