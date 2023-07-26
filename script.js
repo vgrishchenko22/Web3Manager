31146f4eb9fdaed23374f7d5ae5ffc50d1e505c2
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-26 22:31:32 +1000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function begin() {
     WebApp.MainButton.setParams({"text": "Connect", "is_visible": true, "is_active": true});
 }
 
-WebApp.MainButton.onClick(async () => {
+WebApp.MainButton.onClick(() => {
     document.getElementById("connect-section").style.display = "none";
     if (document.getElementById("connect-section").style.display == "block") {}
     if (document.getElementById("search-section").style.display == "block") {}
