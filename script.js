31642c05dc7f084be4e562e87ec16ab388c7001f
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-27 01:05:18 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const WebApp = window.Telegram.WebApp;
 const TonAccess = window.TonAccess;
 const TonWeb = window.TonWeb;
-let tonweb;
-const admin = "EQBeknN9S797M9Bi6Kktf8wREsaIbpMpVDG-EFM5OJ6UkJhp";
+// let tonweb;
+// const admin = "EQBeknN9S797M9Bi6Kktf8wREsaIbpMpVDG-EFM5OJ6UkJhp";
 
-document.addEventListener("DOMContentLoaded", function () {
-    TonAccess.getHttpEndpoint()
-    .then((endpoint) => {
-        tonweb = new TonWeb(new TonWeb.HttpProvider(endpoint));
-    })
-    .catch((error) => console.error(error));
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     TonAccess.getHttpEndpoint()
+//     .then((endpoint) => {
+//         tonweb = new TonWeb(new TonWeb.HttpProvider(endpoint));
+//     })
+//     .catch((error) => console.error(error));
+// });
 
-const tonDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
-    address: "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz",
-});
-const tmeDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
-    address: "EQCA14o1-VWhS2efqoh_9M1b_A9DtKTuoqfmkn83AbJzwnPi",
-});
+// const tonDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
+//     address: "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz",
+// });
+// const tmeDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
+//     address: "EQCA14o1-VWhS2efqoh_9M1b_A9DtKTuoqfmkn83AbJzwnPi",
+// });
 
 function begin() {
     WebApp.MainButton.setParams({"text": "Connect", "is_visible": true, "is_active": true});
