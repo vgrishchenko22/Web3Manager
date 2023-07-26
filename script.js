3feb08cd771bc99d55c15175159eba06c9e7a5b2
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-27 01:14:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const WebApp = window.Telegram.WebApp;
-const TonAccess = window.TonAccess;
-const TonWeb = window.TonWeb;
+// const TonAccess = window.TonAccess;
+// const TonWeb = window.TonWeb;
 // let tonweb;
 // const admin = "EQBeknN9S797M9Bi6Kktf8wREsaIbpMpVDG-EFM5OJ6UkJhp";
 
