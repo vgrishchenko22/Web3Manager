c70a1326f7f435acd11ba1efd1dc6cddfce622c1
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-26 23:41:54 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error(error));
 });
 
-const tonDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
-    address: "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz",
-});
-const tmeDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
-    address: "EQCA14o1-VWhS2efqoh_9M1b_A9DtKTuoqfmkn83AbJzwnPi",
-});
+// const tonDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
+//     address: "EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz",
+// });
+// const tmeDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
+//     address: "EQCA14o1-VWhS2efqoh_9M1b_A9DtKTuoqfmkn83AbJzwnPi",
+// });
 
 function begin() {
     WebApp.MainButton.setParams({"text": "Connect", "is_visible": true, "is_active": true});
