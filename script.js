fb80af14d3f77347141d1e2e0f7c380a8ea079a6
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-26 22:44:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const WebApp = window.Telegram.WebApp;
-const TonAccess = window.TonAccess;
-const TonWeb = window.TonWeb;
 
 function begin() {
-    WebApp.MainButton.setParams({text: "Connect", is_visible: true, is_active: true});
+    WebApp.MainButton.setParams({"text": "Connect", "is_visible": true, "is_active": true});
 }
 
 WebApp.MainButton.onClick(() => {
-    document.getElementById("connect-section").style.display = "none";
-    if (document.getElementById("connect-section").style.display == "block") {}
-    if (document.getElementById("search-section").style.display == "block") {}
-    if (document.getElementById("info-section").style.display == "block") {}
-    if (document.getElementById("edit-section").style.display == "block") {}
-    if (document.getElementById("settings-section").style.display == "block") {}
-}
+    WebApp.MainButton.showProgress();
+    setTimeout(() => {
+        WebApp.showAlert("Connected!", () => {
+            WebApp.MainButton.hideProgress();
+            WebApp.MainButton.setParams({"text": "Find"});
+
+            document.getElementById("connect-section").style.display = "none";
+            document.getElementById("search-section").style.display = "block";
+        });
+    }, 5000);
+});
