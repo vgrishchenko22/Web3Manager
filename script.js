8820972a67ba14557d3bff387f7423df3549b0fa
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-26 22:30:55 +1000

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ const tmeDnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
 function begin() {
     WebApp.MainButton.setParams({"text": "Connect", "is_visible": true, "is_active": true});
 }
+
+WebApp.MainButton.onClick(async () => {
+    document.getElementById("connect-section").style.display = "none";
+    if (document.getElementById("connect-section").style.display == "block") {}
+    if (document.getElementById("search-section").style.display == "block") {}
+    if (document.getElementById("info-section").style.display == "block") {}
+    if (document.getElementById("edit-section").style.display == "block") {}
+    if (document.getElementById("settings-section").style.display == "block") {}
+}
