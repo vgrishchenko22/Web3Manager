526c4b4f01cb6be8d6178a94f8c5f2470803a464
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-27 01:45:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const WebApp = window.Telegram.WebApp;
 // const TonAccess = window.TonAccess;
 // const TonWeb = window.TonWeb;
-// let tonweb;
-// const admin = "EQBeknN9S797M9Bi6Kktf8wREsaIbpMpVDG-EFM5OJ6UkJhp";
+let tonweb;
+const admin = "EQBeknN9S797M9Bi6Kktf8wREsaIbpMpVDG-EFM5OJ6UkJhp";
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     TonAccess.getHttpEndpoint()
