89877db4826dfe36dc8168573e7c31ec4eb857b6
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-26 22:40:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const WebApp = window.Telegram.WebApp;
+const TonAccess = window.TonAccess;
+const TonWeb = window.TonWeb;
+
 function begin() {
-    window.Telegram.WebApp.MainButton.setParams({text: "Connect", is_visible: true, is_active: true});
+    WebApp.MainButton.setParams({text: "Connect", is_visible: true, is_active: true});
 }
 
-window.Telegram.WebApp.MainButton.onClick(() => {
+WebApp.MainButton.onClick(() => {
     document.getElementById("connect-section").style.display = "none";
     if (document.getElementById("connect-section").style.display == "block") {}
     if (document.getElementById("search-section").style.display == "block") {}
