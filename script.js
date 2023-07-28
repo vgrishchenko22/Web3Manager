365d4182bd95102fe08dbc637e1afdb8a98c43da
vgrishchenko22 <98006002+vgrishchenko22@users.noreply.github.com>
2023-07-28 23:26:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error(error));
 });
 
-// function begin() {
-//     WebApp.MainButton.setParams({text: "Search", is_visible: true, is_active: true});
-//     // console.log(document.getElementById("connect-section").style.display, document.getElementById("search-section").style.display, document.getElementById("info-section").style.display, document.getElementById("edit-section").style.display, document.getElementById("settings-section").style.display);
-// }
-
-WebApp.MainButton.setParams({text: "Search", is_visible: true, is_active: true});
-
 async function validateDomain(domain) {
     if (domain.length < 4 || domain.length > 126) {
         WebApp.showAlert("Length error!", () => WebApp.MainButton.hideProgress());
@@ -45,78 +38,91 @@ async function validateDomain(domain) {
 }
 
 WebApp.MainButton.onClick(async () => {
-    WebApp.MainButton.showProgress();
+    if (document.getElementById("search-section").style.display == "block") {
+        console.log("search");
 
-    console.log("search");
+        WebApp.MainButton.showProgress();
 
-    document.getElementById("info-section-domain").innerText = "";
-    document.getElementById("info-section-status").innerText = "";
-    document.getElementById("info-section-news").innerText = "";
+        document.getElementById("info-section-domain").innerText = "";
+        document.getElementById("info-section-status").innerText = "";
+        document.getElementById("info-section-news").innerText = "";
 
-    const domain = document.getElementById('search').value.toLowerCase().trim();
-    const zone = document.getElementById('zone').value;
+        const domain = document.getElementById('search').value.toLowerCase().trim();
+        const zone = document.getElementById('zone').value;
 
-    dnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
-        address: (await tonweb.dns.resolve(domain + zone, TonWeb.dns.DNS_CATEGORY_NEXT_RESOLVER, true)).toString(true, true, true)
-    });
+        dnsCollection = new TonWeb.dns.DnsCollection(tonweb.provider, {
+            address: (await tonweb.dns.resolve(domain + zone, TonWeb.dns.DNS_CATEGORY_NEXT_RESOLVER, true)).toString(true, true, true)
+        });
 
-    if (!(await validateDomain(domain))) {
-        const domainAddress = await dnsCollection.resolve(domain, TonWeb.dns.DNS_CATEGORY_NEXT_RESOLVER, true);
-        const accountInfo = await tonweb.provider.getAddressInfo(domainAddress.toString(true, true, true));
-
-        let domainExists = accountInfo.state === 'active'
-        let ownerAddress = null
+        if (!(await validateDomain(domain))) {
+            const domainAddress = await dnsCollection.resolve(domain, TonWeb.dns.DNS_CATEGORY_NEXT_RESOLVER, true);
+            const accountInfo = await tonweb.provider.getAddressInfo(domainAddress.toString(true, true, true));
     
-        if (domainExists) {
-            dnsItem = new TonWeb.dns.DnsItem(tonweb.provider, {
-                address: domainAddress,
-            })
-            const data = await dnsItem.methods.getData()
-            if (!data.isInitialized) {
-                domainExists = false
-            } else {
-                ownerAddress = data.ownerAddress
-            }
-        }
-        let auctionInfo = null
-        if (domainExists && !ownerAddress) {
-            auctionInfo = await dnsItem.methods.getAuctionInfo()
-            if (auctionInfo.auctionEndTime < Date.now() / 1000) {
-                ownerAddress = auctionInfo.maxBidAddress
-            }
-        }
-        // let lastFillUpTime = 0
-        // if (domainExists && ownerAddress) {
-        //     lastFillUpTime = await dnsItem.methods.getLastFillUpTime()
-        // }
+            let domainExists = accountInfo.state === 'active'
+            let ownerAddress = null
         
-        if (!domainExists) {
-            console.log("free");
-            document.getElementById("info-section-domain").innerText = domain + zone;
-            document.getElementById("info-section-status").innerText = "Status: Available";
-            document.getElementById("info-section-news").innerText = "Soon...";
+            if (domainExists) {
+                dnsItem = new TonWeb.dns.DnsItem(tonweb.provider, {
+                    address: domainAddress,
+                })
+                const data = await dnsItem.methods.getData()
+                if (!data.isInitialized) {
+                    domainExists = false
+                } else {
+                    ownerAddress = data.ownerAddress
+                }
+            }
+            let auctionInfo = null
+            if (domainExists && !ownerAddress) {
+                auctionInfo = await dnsItem.methods.getAuctionInfo()
+                if (auctionInfo.auctionEndTime < Date.now() / 1000) {
+                    ownerAddress = auctionInfo.maxBidAddress
+                }
+            }
+            // let lastFillUpTime = 0
+            // if (domainExists && ownerAddress) {
+            //     lastFillUpTime = await dnsItem.methods.getLastFillUpTime()
+            // }
+            
+            if (!domainExists) {
+                console.log("free");
+                document.getElementById("info-section-domain").innerText = domain + zone;
+                document.getElementById("info-section-status").innerText = "Status: Available";
+                document.getElementById("info-section-news").innerText = "Soon...";
 
-            document.getElementById("search-section").style.display = "none";
-            document.getElementById("info-section").style.display = "block";
-        } else if (ownerAddress) {
-            console.log("busy");
-            document.getElementById("info-section-domain").innerText = domain + zone;
-            document.getElementById("info-section-status").innerText = "Status: Taken";
+                document.getElementById("search-section").style.display = "none";
+                document.getElementById("info-section").style.display = "block";
+            } else if (ownerAddress) {
+                console.log("busy");
+                document.getElementById("info-section-domain").innerText = domain + zone;
+                document.getElementById("info-section-status").innerText = "Status: Taken";
 
-            WebApp.MainButton.hideProgress().setText("Edit");
-            WebApp.BackButton.show();
+                WebApp.MainButton.hideProgress();
+                WebApp.MainButton.setText("Edit");
+                WebApp.BackButton.show();
 
-            document.getElementById("search-section").style.display = "none";
-            document.getElementById("info-section").style.display = "block";
-        } else {
-            console.log("auction");
-            document.getElementById("info-section-domain").innerText = domain + zone;
-            document.getElementById("info-section-status").innerText = "Status: On auction";
-            document.getElementById("info-section-news").innerText = "Soon...";
+                document.getElementById("search-section").style.display = "none";
+                document.getElementById("info-section").style.display = "block";
+            } else {
+                console.log("auction");
+                document.getElementById("info-section-domain").innerText = domain + zone;
+                document.getElementById("info-section-status").innerText = "Status: On auction";
+                document.getElementById("info-section-news").innerText = "Soon...";
 
-            document.getElementById("search-section").style.display = "none";
-            document.getElementById("info-section").style.display = "block";
+                document.getElementById("search-section").style.display = "none";
+                document.getElementById("info-section").style.display = "block";
+            }
         }
+    } else if (document.getElementById("info-section").style.display == "block") {
+        console.log("info");
+
+        document.getElementById("info-section").style.display = "none";
+        document.getElementById("edit-section").style.display = "block";
+    } else if (document.getElementById("edit-section").style.display == "block") {
+        console.log("edit");
+
+        document.getElementById("edit-section").style.display = "none";
+        document.getElementById("settings-section").style.display = "block";
     }
 });
 
